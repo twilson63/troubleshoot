917a3baed2fd79cb44e75566101758508b071b9f
Tom Wilson <twilson63@gmail.com>
2021-06-02 14:45:40 +0000

    update indexjs to contain query examples for cache and data

--- index.js
+++ index.js
@@ -2,8 +2,41 @@ require('dotenv').config()
 const fetch = require('node-fetch')
 const hyper = require('@hyper.io/connect')
 
-console.log(hyper.url('data', 'ckpfg0hyn00k01lvkgmg05w58'))
+fetch(hyper.url('cache', '_query') + '?pattern=4a1dea1b-807b-4cc3-a8c4-29457e34dd96*', {
+  method: 'POST',
+  headers: {
+    Authorization: `Bearer ${hyper.token()}`
+  }
+}).then(r => r.json())
+  .then(r => console.log(r.docs))
 
+//console.log(hyper.url('data', '4a1dea1b-807b-4cc3-a8c4-29457e34dd96'))
+/*
+fetch(hyper.url('data', '_query'), {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    Authorization: `Bearer ${hyper.token()}`
+  },
+  body: JSON.stringify({
+    selector: {
+      type: 'campaign',
+      id: '4a1dea1b-807b-4cc3-a8c4-29457e34dd96'
+    },
+    limit: 2000
+  })
+})
+  .then(r => r.json())
+  .then(result => {
+    result.docs.map(campaign => {
+      console.log({
+        id: campaign.id,
+        limits: campaign.limits,
+        filters: campaign.filters
+      })
+    })
+  })
+*/
 // fetch(hyper.url('data', 'ckpfg0hyn00k01lvkgmg05w58'), {
 //   headers: { Authorization: `Bearer ${hyper.token()}` }
 // })
@@ -30,5 +63,5 @@ function createNewLead() {
     created: { by: 'sys', on: '2021-06-02T12:25:30.016Z' },
     updated: { by: 'sys', on: '2021-06-02T12:25:30.016Z' }
   }
-  
+
 }
